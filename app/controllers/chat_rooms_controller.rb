fd29dd9d602cde9327725b7421cedb56105a8274
Pedro Nascimento <lobo@estudar.com.vc>
2018-04-14 23:20:36 -0300

    Feat: ChatRoom creation & deletion

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,17 +1,31 @@
 class ChatRoomsController < ApplicationController
-  before_action :set_chatroom, only: [:show]
+  before_action :set_chatroom, only: [:show, :destroy]
 
   def index
     @chatrooms = ChatRoom.all
+  end
+
+  def create
+    ChatRoom.create(chat_room_params)
+    redirect_to root_path
   end
 
   def show
     @message = Message.new
   end
 
+  def destroy
+    @chat_room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def set_chatroom
     @chat_room = ChatRoom.find(params[:id])
+  end
+
+  def chat_room_params
+    params.permit(:topic)
   end
 end
